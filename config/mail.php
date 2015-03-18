10e69d11e5be67a10fd61ef1444d933a73ffad04
Martin Sloan <martin.sloan@karma-tek.com>
2015-03-18 12:25:42 -0500

    update email user name

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => 'info@laireight.com', 'name' => 'LairEight Info'],
+	'from' => ['address' => 'info@laireight.com', 'name' => 'USC Todo App'],
 
 	/*
 	|--------------------------------------------------------------------------
